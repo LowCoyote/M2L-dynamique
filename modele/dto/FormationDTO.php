8c3a8edc9462107211f2e2aaed8f7b9673c2e08f
Munderstand <44328530+Munderstand101@users.noreply.github.com>
2021-09-10 14:08:24 +0200

    dto dao

---
+++ modele/dto/FormationDTO.php
@@ -0,0 +1,122 @@
+<?php
+
+
+class FormationDTO
+{
+    private $idForma;
+    private $intitule;
+    private $descriptif;
+    private $duree,
+    private $dateOuvertureInscription;
+    private $dateClotureInscription;
+    private $effectifMax;
+
+
+    /**
+     * FormationDTO constructor.
+     * @param $idForma
+     * @param $intitule
+     * @param $descriptif
+     * @param $duree
+     * @param $dateOuvertureInscription
+     * @param $dateClotureInscription
+     * @param $effectifMax
+     */
+    public function __construct($idForma, $intitule, $descriptif, $duree, $dateOuvertureInscription, $dateClotureInscription, $effectifMax)
+    {
+        $this->idForma = $idForma;
+        $this->intitule = $intitule;
+        $this->descriptif = $descriptif;
+        $this->duree = $duree;
+        $this->dateOuvertureInscription = $dateOuvertureInscription;
+        $this->dateClotureInscription = $dateClotureInscription;
+        $this->effectifMax = $effectifMax;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdForma()
+    {
+        return $this->idForma;
+    }/**
+     * @param mixed $idForma
+     */
+    public function setIdForma($idForma)
+    {
+        $this->idForma = $idForma;
+    }/**
+     * @return mixed
+     */
+    public function getIntitule()
+    {
+        return $this->intitule;
+    }/**
+     * @param mixed $intitule
+     */
+    public function setIntitule($intitule)
+    {
+        $this->intitule = $intitule;
+    }/**
+     * @return mixed
+     */
+    public function getDescriptif()
+    {
+        return $this->descriptif;
+    }/**
+     * @param mixed $descriptif
+     */
+    public function setDescriptif($descriptif)
+    {
+        $this->descriptif = $descriptif;
+    }/**
+     * @return mixed
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }/**
+     * @param mixed $duree
+     */
+    public function setDuree($duree)
+    {
+        $this->duree = $duree;
+    }/**
+     * @return mixed
+     */
+    public function getDateOuvertureInscription()
+    {
+        return $this->dateOuvertureInscription;
+    }/**
+     * @param mixed $dateOuvertureInscription
+     */
+    public function setDateOuvertureInscription($dateOuvertureInscription)
+    {
+        $this->dateOuvertureInscription = $dateOuvertureInscription;
+    }/**
+     * @return mixed
+     */
+    public function getDateClotureInscription()
+    {
+        return $this->dateClotureInscription;
+    }/**
+     * @param mixed $dateClotureInscription
+     */
+    public function setDateClotureInscription($dateClotureInscription)
+    {
+        $this->dateClotureInscription = $dateClotureInscription;
+    }/**
+     * @return mixed
+     */
+    public function getEffectifMax()
+    {
+        return $this->effectifMax;
+    }/**
+     * @param mixed $effectifMax
+     */
+    public function setEffectifMax($effectifMax)
+    {
+        $this->effectifMax = $effectifMax;
+    }
+
+}
--- modele/dto/UtilisateurDTO.php
+++ modele/dto/UtilisateurDTO.php
@@ -3,27 +3,202 @@
 
 class UtilisateurDTO
 {
+    private $idUser;
+    private $nom;
+    private $prenom;
     private $login;
     private $mdp;
+    private $statut;
+    private $typeUser;
+    private $idFonct;
+    private $idLigue;
+    private $idClub;
 
-
-    public function __construct($unLogin = NULL, $unMdp = NULL){
-        $this->login = $unLogin;
-        $this->mdp = $unMdp;
+    /**
+     * UtilisateurDTO constructor.
+     * @param $idUser
+     * @param $nom
+     * @param $prenom
+     * @param $login
+     * @param $mdp
+     * @param $statut
+     * @param $typeUser
+     * @param $idFonct
+     * @param $idLigue
+     * @param $idClub
+     */
+    public function __construct($idUser, $nom, $prenom, $login, $mdp, $statut, $typeUser, $idFonct, $idLigue, $idClub)
+    {
+        $this->idUser = $idUser;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->login = $login;
+        $this->mdp = $mdp;
+        $this->statut = $statut;
+        $this->typeUser = $typeUser;
+        $this->idFonct = $idFonct;
+        $this->idLigue = $idLigue;
+        $this->idClub = $idClub;
     }
 
-    public function getLogin(){
+    /**
+     * @return mixed
+     */
+    public function getIdUser()
+    {
+        return $this->idUser;
+    }
+
+    /**
+     * @param mixed $idUser
+     */
+    public function setIdUser($idUser)
+    {
+        $this->idUser = $idUser;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLogin()
+    {
         return $this->login;
     }
 
-    public function setLogin($unLogin){
-        $this->login = $unLogin;
+    /**
+     * @param mixed $login
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
     }
 
-    public function getMdp(){
+    /**
+     * @return mixed
+     */
+    public function getMdp()
+    {
         return $this->mdp;
     }
-    public function setMdp($unMdp){
-        $this->mdp = $unMdp;
+
+    /**
+     * @param mixed $mdp
+     */
+    public function setMdp($mdp)
+    {
+        $this->mdp = $mdp;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getStatut()
+    {
+        return $this->statut;
+    }
+
+    /**
+     * @param mixed $statut
+     */
+    public function setStatut($statut)
+    {
+        $this->statut = $statut;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypeUser()
+    {
+        return $this->typeUser;
+    }
+
+    /**
+     * @param mixed $typeUser
+     */
+    public function setTypeUser($typeUser)
+    {
+        $this->typeUser = $typeUser;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdFonct()
+    {
+        return $this->idFonct;
+    }
+
+    /**
+     * @param mixed $idFonct
+     */
+    public function setIdFonct($idFonct)
+    {
+        $this->idFonct = $idFonct;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdLigue()
+    {
+        return $this->idLigue;
+    }
+
+    /**
+     * @param mixed $idLigue
+     */
+    public function setIdLigue($idLigue)
+    {
+        $this->idLigue = $idLigue;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdClub()
+    {
+        return $this->idClub;
+    }
+
+    /**
+     * @param mixed $idClub
+     */
+    public function setIdClub($idClub)
+    {
+        $this->idClub = $idClub;
+    }
+
 }
