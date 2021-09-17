02a9ae19bd5e0bacd9518896b906726451659f7b
Pierre Campmas <60074565+LowCoyote@users.noreply.github.com>
2021-09-17 15:23:08 +0200

    Working connection

--- modele/dao/UtilisateurDAO.php
+++ modele/dao/UtilisateurDAO.php
@@ -1,32 +1,4 @@
 <?php
-<<<<<<< Updated upstream
-
-class UtilisateurDAO
-{
-    public static function verification(UtilisateurDTO $utilisateur){
-
-        $requetePrepa = DBConnex::getInstance()->prepare("select * from utilisateur where 
-        login = :login and  mdp = md5(:mdp)");
-
-        $login = $utilisateur->getLogin();
-        $mdp =   $utilisateur->getMdp();
-
-
-        $requetePrepa->bindParam( ":login",$login);
-        $requetePrepa->bindParam( ":mdp" ,  $mdp);
-
-<<<<<<< HEAD
-        $requetePrepa->execute();
-=======
-    public static function verification(UtilisateurDTO $utilisateur){
-
-        $requetePrepa = DBConnex::getInstance()->prepare("select login from utilisateur where 
-        login = :login and  mdp = md5(:mdp)");
-
-        $login = $utilisateur->getLogin();
-        $mdp =   $utilisateur->getMdp();
-
-=======
 use Hydrate;
 
 class UtilisateurDAO
@@ -54,7 +26,6 @@ class UtilisateurDAO
 
         return $result;
     }
->>>>>>> Stashed changes
 
 
     public static function getUtilisateur($username){
@@ -65,17 +36,4 @@ class UtilisateurDAO
         
         return $result;
     }
-<<<<<<< Updated upstream
->>>>>>> 334e40ed392427de192d8e28af762a1537ae4427
-
-        $login = $requetePrepa->fetch(PDO::FETCH_NUM);
-        if(empty($login[0])){
-            return False;
-        }
-        else{
-            return $login;
-        }
-    }
-=======
->>>>>>> Stashed changes
 }
